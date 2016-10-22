848ee2b1c2c39e3b142fe88a5dc046b4741ab521
Swathi8 <swathip@infratab.in>
2016-10-22 13:54:55 +0530

    First step render using react

--- client/app.js
+++ client/app.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../static/components/App.js';
 
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
--- static/components/App.js
+++ static/components/App.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+class App extends React.Component {
+  render () {
+    return (
+      <h1> Hey, Here you go </h1>
+    );
+  }
+}
+
+export default App;
