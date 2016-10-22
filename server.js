2eef7bf448868cd7a527f828ed43454c4eb9de2b
Swathi8 <swathip@infratab.in>
2016-10-22 14:49:29 +0530

    Hot Module loader

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var path = require('path');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
 
 var app = express();
+var compiler = webpack(webpackConfig);
+
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: webpackConfig.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static('./dist'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './client/app.js',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/app.js'
+  ],
   output: {
-    path: './dist',
-    filename: 'app.bundle.js'
+    path: require('path').resolve('./dist'),
+    filename: 'app.bundle.js',
+    publicPath: '/',
   },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['react', 'es2015']
+        presets: ['react', 'es2015'],
+        "env": {
+          "development": {
+            "presets": ["react-hmre"]
+          }
+        }
       }
     }]
   }
