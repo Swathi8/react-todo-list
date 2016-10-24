8b09e9438d5a46d046354965f6d2680e79241229
Swathi8 <swathip@infratab.in>
2016-10-24 10:09:05 +0530

    Change text

--- static/components/App.js
+++ static/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
   render () {
     return (
-      <h1> Hey, Here you go. Yay done! </h1>
+      <h1> This is the App component!! </h1>
     );
   }
 }
